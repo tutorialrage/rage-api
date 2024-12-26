3bdc9d1e553296e904b20837920912341eac1384
tutorialrage <rage0012345@gmail.com>
2024-12-26 11:02:34 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -80,6 +80,7 @@ templateMessage,
 InteractiveMessage,
 Header
 } = require("@whiskeysockets/baileys");
+const { WA_DEFAULT_EPHEMERAL, getAggregateVotesInPollMessage, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, downloadContentFromMessage, areJidsSameUser, getContentType, useMultiFileAuthState, makeWASocket, fetchLatestBaileysVersion, makeCacheableSignalKeyStore, makeWaSocket } = require("baileys")
 const fs = require('fs')
 const util = require('util')
 const cors = require("cors")
