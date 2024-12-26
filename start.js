4e33a1f4fd9e2fd2266bfdbc7a655d8a2a604d48
tutorialrage <rage0012345@gmail.com>
2024-12-26 10:48:16 +0100

    Add files via upload

--- start.js
+++ start.js
@@ -1,141 +1,137 @@
-require("./doc/global");
-const func = require("./doc/place");
+require('../config')
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, getAggregateVotesInPollMessage } = require("@whiskeysockets/baileys");
+const pino = require('pino')
+const { Boom } = require('@hapi/boom')
+const fs = require('fs')
+const FileType = require('file-type')
 const readline = require("readline");
-const yargs = require('yargs/yargs');
-const _ = require('lodash');
-const usePairingCode = true;
-async function fetchJsonMulti(url) {
-const fetch = require("node-fetch")
-const response = await fetch(url);
-if (!response.ok) {
-throw new Error('Network response was not ok');
-}
-return response.json();
-}
+const PhoneNumber = require('awesome-phonenumber')
+const path = require('path')
+const NodeCache = require("node-cache")
+const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+const usePairingCode = global.connect // true pairing // false qr
 const question = (text) => {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    return new Promise((resolve) => {
-        rl.question(text, resolve);
-    });
-};
-
-async function XeonSession() {
-    const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) });
-    const { state, saveCreds } = await useMultiFileAuthState(`./session`);
-    const { version, isLatest } = await fetchLatestBaileysVersion();
-
-    const getMessage = async (key) => {
-        if (store) {
-            const msg = await store.loadMessage(key.remoteJid, key.id, undefined);
-            return msg?.message || undefined;
-        }
-        return { conversation: 'WHATSAPP' };
-    };
-
-    const connectionOptions = {
-        isLatest,
-        getMessage,
-        keepAliveIntervalMs: 30000,
-        printQRInTerminal: !usePairingCode,
-        logger: pino({ level: "fatal" }),
-        auth: state,
-        browser: ['Ubuntu', 'Chrome', '20.0.04']
-    };
-
-    const XeonBotInc = func.makeWASocket(connectionOptions);
-
-    if (usePairingCode && !XeonBotInc.authState.creds.registered) {
-
-const botnumber = await question(chalk.green.bold(`
-
-
-
-
-
-
-
-please enter your bot number, for example: +916909137213:                               
-                                                                           
-        
-
-
-
-
-`));
- {
-console.log(chalk.blue.bold(`🦄 YOUR NUMBER IS CONNECTED SUCCEFUL\n`))
-await sleep(1500)
-const code = await XeonBotInc.requestPairingCode(botnumber.trim())
-console.log(chalk.green.bold(`[ Your WhatsApp Pair Code ] ${code}`))
-}}
-    
-store.bind(XeonBotInc.ev)
-
-XeonBotInc.ev.on('connection.update', async (update) => {
-const { connection, lastDisconnect } = update
-if (connection === 'close') {
-const reason = new Boom(lastDisconnect?.error)?.output.statusCode
-console.log(color(lastDisconnect.error, 'deeppink'))
-if (lastDisconnect.error == 'Error: Stream Errored (unknown)') {
-process.exit()
-} else if (reason === DisconnectReason.badSession) {
-console.log(color(`Bad Session File, Please Delete Session and Scan Again`))
-process.exit()
-} else if (reason === DisconnectReason.connectionClosed) {
-console.log(color('[SYSTEM]', 'white'), color('Connection closed, reconnecting...', 'deeppink'))
-process.exit()
-} else if (reason === DisconnectReason.connectionLost) {
-console.log(color('[SYSTEM]', 'white'), color('Connection lost, trying to reconnect', 'deeppink'))
-process.exit()
-} else if (reason === DisconnectReason.connectionReplaced) {
-console.log(color('Connection Replaced, Another New Session Opened, Please Close Current Session First'))
-XeonBotInc.logout()
-} else if (reason === DisconnectReason.loggedOut) {
-console.log(color(`Device Logged Out, Please Scan Again And Run.`))
-XeonBotInc.logout()
-} else if (reason === DisconnectReason.restartRequired) {
-console.log(color('Restart Required, Restarting...'))
-await XeonSession()
-} else if (reason === DisconnectReason.timedOut) {
-console.log(color('Connection TimedOut, Reconnecting...'))
-XeonSession()
-}
-        } else if (connection === "connecting") {
-            console.log(chalk.blue.bold(`Connecting...`));
-        } else if (connection === "open") {
-console.log(chalk.blue.bold(`Connected to ${XeonBotInc.user.id.split(":")[0]}`));
-            XeonBotInc.sendMessage("916909137213@s.whatsapp.net", { text: `🦄 Endpoint is online!!` });
-        }
-    });
-
-    XeonBotInc.ev.on('messages.upsert', async (chatUpdate) => {
-        try {
-            let m = chatUpdate.messages[0];
-            if (!m.message) return;
-            m.message = (Object.keys(m.message)[0] === 'ephemeralMessage') ? m.message.ephemeralMessage.message : m.message;
-            if (m.key && m.key.remoteJid === 'status@broadcast') return XeonBotInc.readMessages([m.key]);
-            if (!XeonBotInc.public && m.key.remoteJid !== global.owner + "@s.whatsapp.net" && !m.key.fromMe && chatUpdate.type === 'notify') return;
-            if (m.key.id.startsWith('BAE5') && m.key.id.length === 16) return;
-            if (global.autoread) XeonBotInc.readMessages([m.key]);
-            m = func.smsg(XeonBotInc, m, store);
-            require("./server.js")(XeonBotInc, m, store);
-        } catch (err) {
-            console.log(err);
-        }
-    });
-
-    
-    XeonBotInc.public = true;
-
-    XeonBotInc.ev.on('creds.update', saveCreds);
-    return XeonBotInc;
-}
-
-XeonSession();
-
-process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ', err);
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
 });
+return new Promise((resolve) => {
+rl.question(text, resolve)
+})
+};
+//===================
+async function connectToWhatsApp() {
+const { state, saveCreds } = await useMultiFileAuthState("./session")
+const { version, isLatest } = await fetchLatestBaileysVersion();
+const resolveMsgBuffer = new NodeCache()
+const finisher = makeWASocket({
+        logger: pino({ level: "silent" }),
+        printQRInTerminal: !usePairingCode,
+        markOnlineOnConnect: false,
+        auth: state,
+        browser: ["Ubuntu", "Chrome", "20.0.04"]
+    });
+if(usePairingCode && !finisher.authState.creds.registered) {
+        console.log(`Please Input Your Number Bot:`);
+		const phoneNumber = await question(``);		
+		const code = await finisher.requestPairingCode(phoneNumber.trim())
+		console.log(`This Your Pairing Code :`);
+		console.log(`${code}`)
+}
+store.bind(finisher.ev)
+//===================
+finisher.ev.on('call', async (caller) => {
+console.log("Invite to Call")
+})
+finisher.decodeJid = (jid) => {
+if (!jid) return jid
+if (/:\d+@/gi.test(jid)) {
+let decode = jidDecode(jid) || {}
+return decode.user && decode.server && decode.user + '@' + decode.server || jid
+} else return jid
+}
+
+finisher.ev.on('messages.upsert', async chatUpdate => {
+try {
+mek = chatUpdate.messages[0]
+if (!mek.message) return
+mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+if (!finisher.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
+if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
+m = smsg(finisher, mek, store)
+require("../server")(finisher, m, chatUpdate, store)
+} catch (err) {
+console.log(err)
+}
+})
+    
+finisher.getFile = async (PATH, save) => {
+let res
+let data = Buffer.isBuffer(PATH) ? PATH : /^data:.*?\/.*?;base64,/i.test(PATH) ? Buffer.from(PATH.split`,`[1], 'base64') : /^https?:\/\//.test(PATH) ? await (res = await getBuffer(PATH)) : fs.existsSync(PATH) ? (filename = PATH, fs.readFileSync(PATH)) : typeof PATH === 'string' ? PATH : Buffer.alloc(0)
+//if (!Buffer.isBuffer(data)) throw new TypeError('Result is not a buffer')
+let type = await FileType.fromBuffer(data) || {
+mime: 'application/octet-stream',
+ext: '.bin'
+}
+filename = path.join(__filename, '../' + new Date * 1 + '.' + type.ext)
+if (data && save) fs.promises.writeFile(filename, data)
+return {
+res,
+filename,
+size: await getSizeMedia(data),
+...type,
+data
+}}
+finisher.downloadMediaMessage = async (message) => {
+let mime = (message.msg || message).mimetype || ''
+let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+const stream = await downloadContentFromMessage(message, messageType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])}
+return buffer} 
+finisher.sendText = (jid, text, quoted = '', options) => finisher.sendMessage(jid, { text: text, ...options }, { quoted })
+finisher.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
+let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+let buffer
+if (options && (options.packname || options.author)) {
+buffer = await writeExifImg(buff, options)
+} else {
+buffer = await imageToWebp(buff)}
+await finisher.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+return buffer}
+finisher.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+let buffer
+if (options && (options.packname || options.author)) {
+buffer = await writeExifVid(buff, options)
+} else {
+buffer = await videoToWebp(buff)}
+await finisher.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+return buffer}
+finisher.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+let quoted = message.msg ? message.msg : message
+let mime = (message.msg || message).mimetype || ''
+let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+const stream = await downloadContentFromMessage(quoted, messageType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])}
+let type = await FileType.fromBuffer(buffer)
+trueFileName = attachExtension ? (filename + '.' + type.ext) : filename
+// save to file
+await fs.writeFileSync(trueFileName, buffer)
+return trueFileName}
+// Self Public
+finisher.public = global.publicX
+// Connect
+finisher.serializeM = (m) => smsg(finisher, m, store)
+finisher.ev.on('connection.update', (update) => {
+const {connection,lastDisconnect} = update
+if (connection === 'close') {lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut ? connectToWhatsApp() : ''}
+else if(connection === 'open') { console.log('Success Reconected')}
+console.log(update)})
+finisher.ev.on('creds.update', saveCreds)
+}
+connectToWhatsApp()
